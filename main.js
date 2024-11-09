45e6977a316d8709836523cb4d33d37b1e31d2a1
hungryhippoafk <helsing1993@hotmail.com>
2024-11-09 22:13:32 +0100

    mobile menu animation completed. overall work, done

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const menuToggle = document.getElementById('menuToggle');
+const menuToggle = document.querySelector('.menuToggle');
 const mobileNav = document.getElementById('mobileNav');
 const closeToggle = document.getElementById("closeToggle");
 const mobileNavOverlay = document.getElementById("mobileNavOverlay");
-
 
 menuToggle.addEventListener("click",openMenu);
 closeToggle.addEventListener("click",closeMenu);
 
 
-function openMenu(){
-    mobileNavOverlay.style.display ="block";
-    mobileNav.style.opacity = "1";
-    menuToggle.style.display = "none";
-}
+
 function closeMenu(){
-    mobileNavOverlay.style.display="none"
-    mobileNav.style.opacity ="0";
-    menuToggle.style.display ="block";
+  mobileNav.style.display="none";
+  mobileNavOverlay.style.display="none";
+
+}
+
+function openMenu() {
+    mobileNav.style.display ="block";
+    mobileNavOverlay.style.display="block";
+    
 }
